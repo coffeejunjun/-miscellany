e59f83309aeea80c8c96dd057855885cfbceb512
coffeejunjun <coffeesugarsi5@gmail.com>
2019-10-11 13:28:30 +0900

    fix

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  
+curl git clone https://github.com/tomasr/molokai
 cp ./vimrc ~/.vimrc
