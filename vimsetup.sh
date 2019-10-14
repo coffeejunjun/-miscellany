3bd2bad40ffe6c95c49d876d26c06ec79f0c5e3c
coffeejunjun <coffeesugarsi5@gmail.com>
2019-10-14 23:09:04 +0900

    fix

--- vimsetup.sh
+++ vimsetup.sh
@@ -2,6 +2,8 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-git clone https://github.com/tomasr/molokai.git 
+git clone https://github.com/tomasr/molokai.git
+
+mv molokai/colors/molokai.vim ~/.vim/colors/
 
 cp ./vimrc ~/.vimrc
