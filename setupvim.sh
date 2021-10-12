3f2f41ee364a60ffa7f5109156573b35dae02110
coffeejunjun <coffeesugarsi5@gmail.com>
2021-10-12 19:36:45 +0900

    [edit] 自動的に削除する機能の実装

--- setupvim.sh
+++ setupvim.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-echo -e "プラグインの取得中\n"
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2>&1
-
-wait
 cp ./vimrc ~/.vimrc
-echo -e "インストールが完了しました。\nvimを開き :PlugInstall と入力してください。"
+curl -fsLo --create-dirs ~/.vim/autoload/plug.vim \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    echo "An error occurred while retrieving the vim plugin..."
+    exit 1
+fi
+
+echo "Installation is complete !"
+echo "Open vim and type :PlugInstall"
+
+echo "rm -rf $(dirname $0)/"
+rm -rf $(dirname $0)/
