b8c8e61526fd7f580f1c735b6f3f7a9694fe7d63
coffeejunjun <coffeesugarsi5@gmail.com>
2021-10-16 23:47:13 +0900

    [fix] fix bug

--- setupvim.sh
+++ setupvim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp ./vimrc ~/.vimrc
-curl -fsLo --create-dirs ~/.vim/autoload/plug.vim \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2>&1
 
 if [ $? -ne 0 ]; then
